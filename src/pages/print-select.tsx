cd55f68efe210aa1ce616623121921ff10c962d1
PP Chow <admin@Ken-Mac-mini.local>
2025-01-14 18:54:39 +0800

    fix: remove git output from TSX code
    - Removed accidentally added git output text from offline message
    - Updated offline message text
    - Fixed SVG component structure

--- src/pages/print-select.tsx
+++ src/pages/print-select.tsx
@@ -294,15 +294,11 @@ const PrintSelect = () => {
               <div className="flex-shrink-0">
                 <svg className="h-5 w-5 text-yellow-400" viewBox="0 0 20 20" fill="currentColor">
                   <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
-                </svg>To https://github.com/ppchow/pettington-product-navigator.git
-                   4a2a6f1..fc8cc81  main -> main
-                To https://github.com/ppchow/pettington-product-navigator.git
-                   fc8cc81..03a2c35  main -> main
-                
+                </svg>
               </div>
               <div className="ml-3">
                 <p className="text-sm text-yellow-700">
-                  You are currently offline. Showing cached products.
+                  You are currently offline. Some features may be unavailable.
                 </p>
               </div>
             </div>
